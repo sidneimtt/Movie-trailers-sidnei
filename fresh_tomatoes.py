a7674a60be188021db2e52acd89e280cb9a5844f
sidneimtt <sidneim@hotmail.com>
2021-05-30 13:35:04 -0300

    test 3

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -8,13 +8,10 @@ import sys
 main_page_head = '''
 <head>
     <meta charset="utf-8">
-    <title>Movie Trailers</title>       
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Movie Trailers</title>    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>    
     <link rel="stylesheet" type="text/css" href="/static/css/styles.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
