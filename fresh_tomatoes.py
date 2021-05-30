dee96452ac152e631c7283f009134fa27d878401
sidneimtt <sidneim@hotmail.com>
2021-05-30 14:40:35 -0300

    commit h300521

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -2,16 +2,17 @@
 import webbrowser
 import os
 import re
-import sys
+
 
 
 main_page_head = '''
 <head>
     <meta charset="utf-8">
-    <title>Movie Trailers</title>    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>    
-    <link rel="stylesheet" type="text/css" href="/static/css/styles.css" />
+    <title>Movie Trailers</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>          
+    <link rel="stylesheet" type="text/css" href="/static/css/styles.css" />    
 
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
